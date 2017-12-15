1092efeb569b9da326869e6cc252a28ed3351805
Alberto Blanco Vicente <ablancov@imcs.es>
2017-12-15 13:27:38 +0100

    Fixed problem with overflow

--- AOC2017/AOC15/main.rs
+++ AOC2017/AOC15/main.rs
@@ -10,21 +10,32 @@ fn f1() {
     let mut bin_b = format!("{:b}", b);
     let factor_a = 16807;
     let factor_b = 48271;
-   //for _ in 0..40000000 {
-    for _ in 0..4 {
+    let mut total = 0;
+    for _ in 0..40000000 {
+    //for _ in 0..4 {
         a = next_value(a, factor_a);
         b = next_value(b, factor_b);
-         println!("{}...{}", a, b);
+        //println!("{}...{}",a,b);
         let aux_a = format!("{:b}", a);
-        let ida = aux_a.len() - 16;
+        let ida = if aux_a.len() > 16 {
+            aux_a.len() - 16
+        } else {
+            0
+        };
         let final_a = &aux_a[ida..];
         let aux_b = format!("{:b}", b);
-        let idb = aux_b.len() - 16;
+        //let idb = aux_b.len() - 16;
+        let idb = if aux_b.len() > 16 {
+            aux_b.len() - 16
+        } else {
+            0
+        };
         let final_b = &aux_b[idb..];
         if (final_a == final_b) {
-            println!("...");
+            total = total + 1;
         }
     }
+    println!("{}", total);
 }
 
 fn main() {
